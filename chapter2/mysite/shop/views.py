e6d5e05b0a3d1670c263ea5dc0ff66cc046cbe69
fatemehmirzapoor <mirzapoor98@gmail.com>
2021-06-06 13:09:54 +0430

    Tags -v3

--- chapter2/mysite/shop/views.py
+++ chapter2/mysite/shop/views.py
@@ -5,8 +5,7 @@ from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
-
-
+from django.db.models import Count
 
 def shop_list(request,tag_slug=None):
         object_list = shop.objects.all()
@@ -57,7 +56,16 @@ def shop_detail(request, year, month, day, post):
                         new_comment.save()
         else:
                 comment_form = CommentForm()
-        return render(request,'shop/post/detali.html',{'post': post,'comments': comments,'new_comment': new_comment,'comment_form': comment_form})
+
+        # List of similar posts
+        post_tags_ids = shop.tags.values_list('id', flat=True)
+        similar_posts = shop.objects.filter(tags__in=post_tags_ids) .exclude(id=post.id)
+        similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+        return render(request,'shop/post/detali.html',{'post': post,'comments': comments ,       
+                                                        'new_comment': new_comment,
+                                                        'comment_form': comment_form,
+                                                        'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
