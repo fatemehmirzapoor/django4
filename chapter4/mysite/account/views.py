5ae2a19f241095c31d6708e079317b0bfee788d2
fatemehmirzapoor <mirzapoor98@gmail.com>
2021-06-29 18:04:47 +0430

    pillow -v2

--- chapter4/mysite/account/views.py
+++ chapter4/mysite/account/views.py
@@ -2,7 +2,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm,UserRegistrationForm,UserEditForm, ProfileEditForm
+from .forms import LoginForm,UserRegistrationForm,\
+                                        UserEditForm,ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 
@@ -43,9 +44,11 @@ def register(request):
             new_user.set_password(
                     user_form.cleaned_data['password'])
             # Save the User object
-            # Create the user profile
-            Profile.objects.create(user=new_user)
             new_user.save()
+            # Create the user profile
+
+            Profile.objects.create(user=new_user)
+
             return render(request,
                             'account/register_done.html',
                             {'new_user': new_user})
@@ -69,7 +72,8 @@ def edit(request):
             profile_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
+        profile_form = ProfileEditForm(
+                        instance=request.user.profile)
     return render(request,
                     'account/edit.html',
                     {'user_form': user_form,
